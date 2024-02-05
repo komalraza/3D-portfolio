030a78869b6241e1f316e0995ca8ec3e06cf4145
razaakomal <komal.raza@leoatech.com>
2024-02-05 09:57:30 +0500

    3D model display in hero section

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { useProgress, Html } from "@react-three/drei";
 
 const Loader = () => {
-  return (
-    <div>Loader</div>
-  )
-}
+  const { progress } = useProgress();
 
-export default Loader
+  return (
+    <Html>
+      <span className="canvas-load"></span>
+      <p
+        style={{
+          fontSize: 14,
+          fontWeight: 800,
+          marginTop: 40,
+          color: "#f1f1f1",
+        }}
+      >
+        {progress.toFixed(2)}%
+      </p>
+    </Html>
+  );
+};
+
+export default Loader;
